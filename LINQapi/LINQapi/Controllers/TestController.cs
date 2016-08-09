031689d45cdf0f0b218c6406da959eb417986cea
Marcin O <marcin.obiedz@gmail.com>
2016-08-10 00:17:35 +0200

    Added test controller

---
+++ LINQapi/LINQapi/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LINQapi.Controllers
+{
+    public class TestController : ApiController
+    {
+        public void get(int id)
+        {
+            Console.Write(id);
+        }
+    }
+}
