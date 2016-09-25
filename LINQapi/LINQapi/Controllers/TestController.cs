20859774d5fb09966110cc5aa8232430ab3d2ab4
Marcin <marcin.obiedz@gmail.com>
2016-09-25 14:05:36 +0200

    Along with previous commit, code cleanup updating gitignore, specified for VS2015

---
+++ LINQapi/LINQapi/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using LINQapi.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LINQapi.Controllers
+{
+    public class TestController : ApiController
+    {
+        public void Get(int id)
+        {
+            var db = new MyDbSet();
+            //var aaa = db.Customers.Where(cus => cus.CustomerID > 5).AsQueryable().Expression;
+            //var bb = db.Customers.Where(cus => cus.CustomerID > 50)
+            //var a = "db.Customers.Where(cus=>cus.FirstName == \"Alan\").Take(2)";
+            var test = new ExpressionGenerator();
+            test.GenerateExpression("db.Customers.AsQueryable().Where(cus => cus.CustomerID > 5)", db);
+            //var a1 = test.Execute(a);
+            //test.Execute(a);
+        }
+    }
+}
---
+++ LINQapi/LINQapi/Helpers/CodeExample.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Linq.Expressions;
+
+namespace LINQapi.Helpers
+{
+    public class CodeExample
+    {
+        //private LINQapi.AdventureWorksLT2012Entities db = new LINQapi.AdventureWorksLT2012Entities();
+        public void Execute()
+        {
+            //var prodCat = db.ProductCategories.ToList();
+            //var prod = db.Products.ToList();
+            //return prod.AsQueryable().Expression;
+        }
+    }
+}
