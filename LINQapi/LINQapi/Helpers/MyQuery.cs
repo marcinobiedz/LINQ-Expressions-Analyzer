2c085865031f45754eadef4a9aa6b1d5bd290b2a
Marcin <marcin.obiedz@gmail.com>
2016-11-12 13:03:58 +0100

    Code quality

--- LINQapi/LINQapi/Helpers/MyQuery.cs
+++ LINQapi/LINQapi/Helpers/MyQuery.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
+﻿using System.Linq;
 namespace LINQapi.Helpers
 {
-    public class Test
+    public class MyQuery
     {
+        public IQueryable<object> result(MyDbSet db)
+        {
+            IQueryable<object>[] expressionsSet = new IQueryable<object>[10];
+            for (int i = 0; i < 10; i++)
+            {
+                var temp = db.Customers.Take();
+            }
+            return " + originalQueryFromWeb + "; "
+        }
     }
 }
