4e2ba392629da2fbbb916f5f34d0637af648c13b
Marcin <marcin.obiedz@gmail.com>
2016-11-30 00:01:14 +0100

    Separate chart resp from tree resp, first sketches

---
+++ LINQapi/LINQapi/Controllers/ChartController.cs
@@ -0,0 +1,21 @@
+﻿using LINQapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace LINQapi.Controllers
+{
+    public class ChartController : ApiController
+    {
+        [EnableCors(origins: "http://localhost:63342", headers: "*", methods: "*")]
+        [HttpPost]
+        public ChartResponseModel Post([FromBody] string fromWeb)
+        {
+            return new ChartResponseModel();
+        }
+    }
+}
---
+++ LINQapi/LINQapi/Models/ChartResponseModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace LINQapi.Models
+{
+    public class ChartResponseModel
+    {
+        public ChartResponseModel()
+        {
+            isResponseValid = false;
+            errors = new List<string>();
+        }
+
+        [DataMember(IsRequired = true)]
+        public bool isResponseValid { get; set; }
+
+        [DataMember]
+        public List<string> errors { get; set; }
+
+        [DataMember]
+        public int[] initialCounts { get; set; }
+
+        [DataMember]
+        public int[] finalCounts { get; set; }
+
+        [DataMember]
+        public long[] executionTimes { get; set; }
+
+        [DataMember]
+        public TableInfoModel[] tablesInfo { get; set; }
+    }
+}
