6d7b89a5b81784726caf44b1aec6765b1bacf273
Marcin O <marcin.obiedz@gmail.com>
2016-12-11 00:32:50 +0100

    Added constants and test DB files

--- LINQapi/LINQapi/Helpers/Constants.cs
+++ LINQapi/LINQapi/Helpers/Constants.cs
@@ -8,6 +8,8 @@ namespace LINQapi.Helpers
     public class Constants
     {
         public static MyDbSet db;
+        public static readonly string DB_PATH = @"Database\";
+        public static readonly string TEST_DB_PATH = @"TestDatabase\";
         public static readonly int DB_DIVIDER = 10;
         public static readonly string FROM_WEB_REF = "db.SalesOrderDetails.Where(sod => sod.Product.ProductNumber.StartsWith(\"aaa\")).Select(ob=>ob.UnitPrice)";
         public static readonly string FROM_WEB = "db.SalesOrderDetails.Join(db.SalesOrderHeaders," +
