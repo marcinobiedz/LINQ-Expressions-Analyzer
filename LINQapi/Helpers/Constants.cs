148e1c06cf5259a051ad6a6754380b8205e52e5a
Marcin <marcin.obiedz@gmail.com>
2017-03-03 21:22:14 +0100

    Rest of settings moved to the configuration

--- LINQapi/Helpers/Constants.cs
+++ LINQapi/Helpers/Constants.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 
@@ -8,13 +9,7 @@ namespace LINQapi.Helpers
     public class Constants
     {
         public static MyDbSet db;
-        public static readonly int DB_DIVIDER = 10;
-        public static readonly string FROM_WEB_REF = "db.SalesOrderDetails.Where(sod => sod.Product.ProductNumber.StartsWith(\"aaa\")).Select(ob=>ob.UnitPrice)";
-        public static readonly string FROM_WEB = "db.SalesOrderDetails.Join(db.SalesOrderHeaders," +
-            "sod => sod.SalesOrderID," +
-            "soh => soh.SalesOrderID," +
-            "(sod, soh) => new { ID = sod.SalesOrderDetailID, An = soh.AccountNumber })" +
-            ".Where(obj => obj.An.StartsWith(\"10-4020-0002\"))" +
-            ".Select(sel => sel.ID)";
+        public static readonly int DB_DIVIDER = int.Parse(ConfigurationManager.AppSettings["DB_DIVIDER"]);
+        public static readonly string FROM_WEB_REF = ConfigurationManager.AppSettings["FROM_WEB_REF"];
     }
 }
