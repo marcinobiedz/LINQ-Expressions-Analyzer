a23133a0def03223276f10293d29f818546b2568
Marcin O <marcin.obiedz@gmail.com>
2017-03-19 19:11:36 +0100

    Removed typo

--- LINQapi/Analyzer/ExpressionTreeVizualizer.cs
+++ LINQapi/Analyzer/ExpressionTreeVizualizer.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 
-namespace LINQapi.Helpers
+namespace LINQapi.Analyzer
 {
     public class ExpressionTreeVizualizer
     {
